3290e924d25c83d2a4c07c03f0f569f92f07dfe4
Hernan Garchtrom <hernan.garchtrom@rga.com>
2016-08-26 16:53:59 -0300

    CurriedAdd

---
+++ currying.js
@@ -0,0 +1,22 @@
+// Currying example in Javascript
+// The following example is done for just a simple add functionality
+function curriedAdd(x, y) {
+  if(arguments.length === 0) {
+    // In case we don't receive any parameter we simply return the function as it is,
+    // expecting two values to add.
+    return curriedAdd;
+  } else if(arguments.length === 1) {
+    // Instead if we receive only one parameter, we still need a second one to
+    // perform the operation, so we return an anonymous function expecting
+    // just one parameter, because the first part for the sum has already
+    // been received on the first call.
+    return z => {
+      return x + z;
+    }
+  }
+  return x + y;
+}
+
+var c1 = curriedAdd(); // typeof c1 === 'function'
+var c2 = curriedAdd(2); // typeof c2 === 'function' here we have a partial application of curriedAdd
+var c3 = f2(3); // c3 === 5
